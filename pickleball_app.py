d80d63a82f426e4e413a40de531c85e6eba5a526
haydenmchan <128524334+haydenmchan@users.noreply.github.com>
2023-06-02 18:50:51 -0500

    Update pickleball_app.py

--- pickleball_app.py
+++ pickleball_app.py
@@ -5,4 +5,7 @@ import requests
 streamlit.title('Pickleball Tournament Bracket Maker')
 
 streamlit.header('Enter teams here')
-playing_pars = streamlit.text_input('Enter player pairs')
+playing_pairs = streamlit.text_input('Enter player pairs. Seperate teams with a ","')
+
+streamlit.body('Here are the teams for tonights match')
+streamlit.datadframe(playing_pairs)
