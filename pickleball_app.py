b26ed7a364c890abe8c62f22cf9a54603962cd17
haydenmchan <128524334+haydenmchan@users.noreply.github.com>
2023-06-02 20:35:54 -0500

    Update pickleball_app.py

--- pickleball_app.py
+++ pickleball_app.py
@@ -1,31 +1,19 @@
-import pandas
-import requests
+# streamlit_app.py
+
+import pandas as pd
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
-
-# Perform SQL query on the Google Sheet.
+# Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
-for row in rows:
+for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
 
 
